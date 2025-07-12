a8cc248f4eee73d9291c12cce3366bc7672c626b
Javier Jimenez Shaw <j1@jimenezshaw.com>
2025-07-12 12:54:40 +0200

    Dev to master (#4)
    First useful commit
    ---------
    
    Co-authored-by: OSHistory <9952252+OSHistory@users.noreply.github.com>

---
+++ helmert_calc.py
@@ -0,0 +1,279 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import math
+
+import numpy as np
+import pyproj
+
+
+def umeyama(X, Y):
+    # https://github.com/clementinboittiaux/umeyama-python
+    """
+    Estimates the Sim(3) transformation between `X` and `Y` point sets.
+
+    Estimates c, R and t such as c * R @ X + t ~ Y.
+
+    Parameters
+    ----------
+    X : numpy.array
+        (m, n) shaped numpy array. m is the dimension of the points,
+        n is the number of points in the point set.
+    Y : numpy.array
+        (m, n) shaped numpy array. Indexes should be consistent with `X`.
+        That is, Y[:, i] must be the point corresponding to X[:, i].
+
+    Returns
+    -------
+    c : float
+        Scale factor.
+    R : numpy.array
+        (3, 3) shaped rotation matrix.
+    t : numpy.array
+        (3, 1) shaped translation vector.
+    """
+    mu_x = X.mean(axis=1).reshape(-1, 1)
+    mu_y = Y.mean(axis=1).reshape(-1, 1)
+    var_x = np.square(X - mu_x).sum(axis=0).mean()
+    cov_xy = ((Y - mu_y) @ (X - mu_x).T) / X.shape[1]
+    U, D, VH = np.linalg.svd(cov_xy)
+    S = np.eye(X.shape[0])
+    if np.linalg.det(U) * np.linalg.det(VH) < 0:
+        S[-1, -1] = -1
+    c = np.trace(np.diag(D) @ S) / var_x
+    R = U @ S @ VH
+    t = mu_y - c * R @ mu_x
+    return c, R, t
+
+
+def solve_helmert(geoc_s, geoc_t):
+    c, R, t = umeyama(np.transpose(geoc_s), np.transpose(geoc_t))
+    # print("\nc", c, "\nR", R, "\nt", t)
+    secs = 3600
+    x = t[0][0]
+    y = t[1][0]
+    z = t[2][0]
+    rx = math.degrees(R[2, 1]) * secs
+    ry = math.degrees(R[0, 2]) * secs
+    rz = math.degrees(R[1, 0]) * secs
+    s = (c - 1) * 1e6
+    return (x, y, z, rx, ry, rz, s)
+
+
+def geographic_to_geocentric(ellipsoid_str, llhs):
+    pipeline = (
+        "+proj=pipeline "
+        "+step +proj=axisswap +order=2,1 "
+        "+step +proj=unitconvert +xy_in=deg +xy_out=rad "
+        f"+step +proj=cart {ellipsoid_str}"
+    )
+    tr = pyproj.transformer.Transformer.from_pipeline(pipeline)
+    res = []
+    for llh in llhs:
+        res.append(np.array(tr.transform(*llh)))
+    return np.array(res)
+
+
+def _read_ellipsoid(ellip):
+    # Check if ellipsoid has +a and +rf defined, if not read from proj list
+    # Include custom: boolean to determine in the pipeline if "+a and +rf"
+    # or +ellps parameter is to be used
+    el = None
+    if "a" not in ellip or "rf" not in ellip:
+        # print(f"Reading ellipsoid {ellip['name']} from pyproj")
+        # https://proj.org/en/stable/usage/ellipsoids.html#ellipsoids
+        ellps = pyproj.list.get_ellps_map()
+        if ellip["name"] in ellps.keys():
+            el_in_map = ellps[ellip["name"]]
+            el = {
+                "name": ellip["name"],
+                "a": el_in_map["a"],
+                "rf": el_in_map["rf"],
+                "custom": False,
+            }
+            return el
+    # no lookup necessary -> return user defined variables
+    else:
+        return {
+            "name": ellip["name"],
+            "a": ellip["a"],
+            "rf": ellip["rf"],
+            "custom": True,
+        }
+
+
+def _make_pipeline(pipe, ellps_s_str, ellps_t_str, pm_s_str, pm_t_str, is2D=False):
+    p = (
+        "+proj=pipeline "
+        " +step +proj=axisswap +order=2,1 "
+        " +step +proj=unitconvert +xy_in=deg +xy_out=rad "
+        f" {pm_s_str} "
+        f" {'+step +proj=push +v_3' if is2D else ''} "
+        f" +step +proj=cart {ellps_s_str} "
+        f" {pipe} "
+        f" +step +inv +proj=cart {ellps_t_str} "
+        f" {'+step +proj=pop +v_3' if is2D else ''} "
+        f" {pm_t_str} "
+        " +step +proj=unitconvert +xy_in=rad +xy_out=deg "
+        " +step +proj=axisswap +order=2,1"
+    )
+    return p
+
+
+def _build_ellipsoid_parameters(ellipsoid):
+    if ellipsoid["custom"]:
+        return f"+a={ellipsoid['a']} +rf={ellipsoid['rf']}"
+    else:
+        return f"+ellps={ellipsoid['name']}"
+
+
+def _make_solution(x, y, z, rx, ry, rz, s, ellps_s_str, ellps_t_str, pm_s, pm_t):
+    sol = {}
+    helmert = sol["helmert"] = {}
+    helmert["params"] = {"x": x, "y": y, "z": z, "rx": rx, "ry": ry, "rz": rz, "s": s}
+    pipe = (
+        f"+step +proj=helmert "
+        f"+x={x:.3f} +y={y:.3f} +z={z:.3f} "
+        f"+rx={rx:.4f} +ry={ry:.4f} +rz={rz:.4f} "
+        f"+s={s:.3f} +convention=position_vector"
+    )
+
+    pm_s_str = f"+step +inv +proj=longlat +pm={pm_s}" if pm_s else ""
+    pm_t_str = f"+step +proj=longlat +pm={pm_t}" if pm_t else ""
+
+    helmert["string"] = pipe
+    helmert["pipeline_2D"] = _make_pipeline(
+        pipe,
+        ellps_s_str,
+        ellps_t_str,
+        pm_s_str,
+        pm_t_str,
+        True,
+    )
+    helmert["pipeline_3D"] = _make_pipeline(
+        pipe,
+        ellps_s_str,
+        ellps_t_str,
+        pm_s_str,
+        pm_t_str,
+        False,
+    )
+    return sol
+
+
+def _pm_correction(points, pm):
+    if pm:
+        pipeline = (
+            "+proj=pipeline "
+            "+step +proj=axisswap +order=2,1 "
+            f"+step +inv +proj=longlat +pm={pm} "
+            "+step +proj=axisswap +order=2,1 "
+        )
+    else:
+        pipeline = "+proj=noop"
+
+    tr = pyproj.transformer.Transformer.from_pipeline(pipeline)
+    res = []
+    for point in points:
+        res.append(np.array(tr.transform(*point)))
+    return np.array(res)
+
+
+def _read_input(obj):
+    ellps = _read_ellipsoid(obj["ellipsoid"])
+    pm = obj.get("pm")
+    points = _pm_correction(obj["points"], pm)
+    ellipsoid_str = _build_ellipsoid_parameters(ellps)
+    geoc = geographic_to_geocentric(ellipsoid_str, points)
+    return ellipsoid_str, pm, geoc
+
+
+def compute_residuals(input, sol):
+    src = np.array(input["source"]["points"])
+    tgt = np.array(input["target"]["points"])
+    center = np.mean(tgt, axis=0)
+
+    ellps = _build_ellipsoid_parameters(_read_ellipsoid(input["target"]["ellipsoid"]))
+    pipeline_tmerc = (
+        "+proj=pipeline +step +proj=axisswap +order=2,1 "
+        "+step +proj=unitconvert +xy_in=deg +xy_out=rad "
+        f"+step +proj=tmerc {ellps} +lat_0={center[0]} +lon_0={center[1]}"
+    )
+    tr_tmerc = pyproj.transformer.Transformer.from_pipeline(pipeline_tmerc)
+
+    def calc_residuals(key):
+        pipeline_to_tgt = sol["helmert"][key]
+        tr_to_tgt = pyproj.transformer.Transformer.from_pipeline(pipeline_to_tgt)
+        converted = [np.array(tr_to_tgt.transform(*p)) for p in src]
+
+        res_1 = [np.array(tr_tmerc.transform(*p)) for p in converted]
+        res_2 = [np.array(tr_tmerc.transform(*p)) for p in tgt]
+
+        residuals = [(r2 - r1).tolist() for r1, r2 in zip(res_1, res_2)]
+        return residuals
+
+    res_2D = [np.linalg.norm(np.array(p[:2])) for p in calc_residuals("pipeline_2D")]
+    res_3D = calc_residuals("pipeline_3D")
+    result = {
+        "pipeline_3D": res_3D,
+        "pipeline_2D": res_2D,
+        "rms_2D": np.sqrt(np.mean(np.square(res_2D))),
+    }
+    return result
+
+
+def helmert_calc(input):
+    ellps_s, pm_s, geoc_s = _read_input(input["source"])
+    ellps_t, pm_t, geoc_t = _read_input(input["target"])
+    hlmrt = solve_helmert(geoc_s, geoc_t)
+
+    sol = _make_solution(*hlmrt, ellps_s, ellps_t, pm_s, pm_t)
+    return sol
+
+
+def main(input_filename, output_filename, inverse, quiet):
+    with open(input_filename) as input_f:
+        input = json.load(input_f)
+
+    if inverse:
+        input["source"], input["target"] = input["target"], input["source"]
+
+    solution = helmert_calc(input)
+    residuals = compute_residuals(input, solution)
+    if not quiet:
+        print("solution:", json.dumps(solution, indent=4))
+        print("residuals:", json.dumps(residuals, indent=4))
+
+    if output_filename:
+        res = input
+        res["solution"] = solution
+        res["residuals"] = residuals
+        with open(output_filename, "w") as output_f:
+            json.dump(res, output_f, indent=4)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Calculate Helmert parameters.")
+    parser.add_argument(
+        "--input", "-i", type=str, help="file path for input values as JSON"
+    )
+    parser.add_argument(
+        "--output", "-o", type=str, help="file path for output values as JSON"
+    )
+    parser.add_argument(
+        "--inverse",
+        default=False,
+        action=argparse.BooleanOptionalAction,
+        help="invert source and target",
+    )
+    parser.add_argument(
+        "--quiet",
+        "-q",
+        default=False,
+        action=argparse.BooleanOptionalAction,
+        help="file path for output values as JSON",
+    )
+    args = parser.parse_args()
+
+    main(args.input, args.output, args.inverse, args.quiet)
---
+++ tests/test_helmert_cal.py
@@ -0,0 +1,259 @@
+import math
+import os
+import shlex
+import subprocess
+import tempfile
+
+import pyproj
+import pytest
+
+import helmert_calc as hc
+
+
+def basic_dhdn_to_etrs89():
+    input = {
+        "source": {
+            "ellipsoid": {"name": "bessel"},
+            "points": [[53, 8, 0], [52, 13, 0], [50, 12, 0], [47, 7, 0], [49, 10, 0]],
+        },
+        "target": {
+            "ellipsoid": {"name": "GRS80"},
+            "points": [
+                [52.9985065862, 7.9990898704, 41.7584],
+                [51.9986488003, 12.9983316939, 42.5789],
+                [49.9988693078, 11.99852119, 48.0281],
+                [46.9991910348, 6.9992606373, 55.158],
+                [48.9989715037, 9.9988250827, 51.0665],
+            ],
+        },
+    }
+    return input
+
+
+def mgi_ferro_to_etrs89():
+    input = {
+        "source": {
+            "pm": "ferro",
+            "ellipsoid": {"name": "bessel"},
+            "points": [
+                [48.2200979, 33.3867950, 164.0],
+                [48.2223853, 33.3510895, 162.0],
+                [48.2027103, 33.3442230, 171.4],
+                [48.1949297, 33.3661957, 160.5],
+                [48.2104897, 33.3970947, 162.7],
+            ],
+        },
+        "target": {
+            "ellipsoid": {"name": "GRS80"},
+            "points": [
+                [48.2195734529, 15.7190118888, 208.9874],
+                [48.2218593701, 15.6833110853, 207.0136],
+                [48.2021864424, 15.6764470206, 216.4388],
+                [48.1944075001, 15.6984173143, 205.5290],
+                [48.2099667290, 15.7293109162, 207.6887],
+            ],
+        },
+    }
+    return input
+
+
+def swap(input):
+    input["source"], input["target"] = input["target"], input["source"]
+    return input
+
+
+def test_nothing():
+    assert True
+
+
+def test_mirrored_points():
+    # the points are not properly paired
+    input = {
+        "source": {
+            "ellipsoid": {"name": "GRS80"},
+            "points": [[53, 8, 0], [52, 13, 0], [50, 12, 0]],
+        },
+        "target": {
+            "ellipsoid": {"name": "GRS80"},
+            "points": [[53, 8, 0], [50, 12, 0], [52, 13, 0]],
+        },
+    }
+    sol = hc.helmert_calc(input)
+    print(sol)
+    assert math.fabs(sol["helmert"]["params"]["s"]) > 1000
+
+
+@pytest.mark.parametrize("inverse", [False, True])
+def test_dhdn_to_etrs89(inverse):
+    # https://epsg.org/transformation_1776/DHDN-to-ETRS89-2.html
+
+    input = basic_dhdn_to_etrs89()
+    sign = 1
+    if inverse:
+        sign = -1
+        input = swap(input)
+
+    sol = hc.helmert_calc(input)
+    params = sol["helmert"]["params"]
+    assert sign * params["x"] == pytest.approx(598.10, abs=0.01)
+    assert sign * params["y"] == pytest.approx(73.70, abs=0.01)
+    assert sign * params["z"] == pytest.approx(418.20, abs=0.01)
+    assert sign * params["rx"] == pytest.approx(0.202, abs=0.001)
+    assert sign * params["ry"] == pytest.approx(0.045, abs=0.001)
+    assert sign * params["rz"] == pytest.approx(-2.455, abs=0.001)
+    assert sign * params["s"] == pytest.approx(6.70, abs=0.001)
+
+
+@pytest.mark.parametrize("inverse", [False, True])
+def test_dhdn_to_etrs89_pyproj(inverse):
+    input = basic_dhdn_to_etrs89()
+    if inverse:
+        input = swap(input)
+
+    sol = hc.helmert_calc(input)
+
+    tr = pyproj.transformer.Transformer.from_pipeline(sol["helmert"]["pipeline_3D"])
+    res = []
+    for llh in input["source"]["points"]:
+        res.append(tr.transform(*llh))
+    for computed, data in zip(res, input["target"]["points"]):
+        assert computed[0] == pytest.approx(data[0], abs=1e-7)
+        assert computed[1] == pytest.approx(data[1], abs=1e-7)
+        assert computed[2] == pytest.approx(data[2], abs=1e-3)
+
+
+@pytest.mark.parametrize("inverse", [False, True])
+def test_dhdn_to_etrs89_ellipsoid_by_params(inverse):
+    input = basic_dhdn_to_etrs89()
+    assert input["source"]["ellipsoid"]["name"] == "bessel"
+
+    input["source"]["ellipsoid"]["name"] = "change name"
+    input["source"]["ellipsoid"]["a"] = 6377397.155
+    input["source"]["ellipsoid"]["rf"] = 299.1528128
+    assert input["source"]["ellipsoid"]["name"] != "bessel"
+
+    if inverse:
+        input = swap(input)
+        assert input["target"]["ellipsoid"]["name"] == "change name"
+
+    sol = hc.helmert_calc(input)
+
+    tr = pyproj.transformer.Transformer.from_pipeline(sol["helmert"]["pipeline_3D"])
+    res = []
+    for llh in input["source"]["points"]:
+        res.append(tr.transform(*llh))
+    for computed, data in zip(res, input["target"]["points"]):
+        assert computed[0] == pytest.approx(data[0], abs=1e-7)
+        assert computed[1] == pytest.approx(data[1], abs=1e-7)
+        assert computed[2] == pytest.approx(data[2], abs=1e-3)
+
+
+@pytest.mark.parametrize(
+    "prime_meridian",
+    [
+        "ferro",
+        -17.6666666666,
+        pytest.param(-17, marks=pytest.mark.xfail),
+        pytest.param("foo", marks=pytest.mark.xfail),
+    ],
+)
+def test_mgi_ferro_to_etrs89(prime_meridian):
+    # https://epsg.org/transformation_1619/MGI-to-ETRS89-1.html
+    input = mgi_ferro_to_etrs89()
+    input["source"]["pm"] = prime_meridian
+    sol = hc.helmert_calc(input)
+
+    tr = pyproj.transformer.Transformer.from_pipeline(sol["helmert"]["pipeline_3D"])
+    res = []
+    for llh in input["source"]["points"]:
+        res.append(tr.transform(*llh))
+    for computed, data in zip(res, input["target"]["points"]):
+        assert computed[0] == pytest.approx(data[0], abs=1e-7)
+        assert computed[1] == pytest.approx(data[1], abs=1e-7)
+        assert computed[2] == pytest.approx(data[2], abs=2e-3)
+
+    params = sol["helmert"]["params"]
+    assert params["x"] == pytest.approx(577.326, abs=0.04)
+    assert params["y"] == pytest.approx(90.129, abs=0.11)
+    assert params["z"] == pytest.approx(463.919, abs=0.05)
+    assert params["rx"] == pytest.approx(5.137, abs=0.003)
+    assert params["ry"] == pytest.approx(1.474, abs=0.002)
+    assert params["rz"] == pytest.approx(5.297, abs=0.002)
+    assert params["s"] == pytest.approx(2.4232, abs=0.001)
+
+
+@pytest.mark.parametrize("prime_meridian", ["ferro", -17.6666666666])
+def test_mgi_ferro_to_etrs89_inverted(prime_meridian):
+    # https://epsg.org/transformation_1619/MGI-to-ETRS89-1.html
+    input = mgi_ferro_to_etrs89()
+    input["source"]["pm"] = prime_meridian
+
+    input = swap(input)
+    sol = hc.helmert_calc(input)
+
+    tr = pyproj.transformer.Transformer.from_pipeline(sol["helmert"]["pipeline_3D"])
+    res = []
+    for llh in input["source"]["points"]:
+        res.append(tr.transform(*llh))
+    for computed, data in zip(res, input["target"]["points"]):
+        assert computed[0] == pytest.approx(data[0], abs=1e-8)
+        assert computed[1] == pytest.approx(data[1], abs=1e-8)
+        assert computed[2] == pytest.approx(data[2], abs=2e-3)
+
+    params = sol["helmert"]["params"]
+    assert params["x"] == pytest.approx(-577.326, abs=0.04)
+    assert params["y"] == pytest.approx(-90.129, abs=0.11)
+    assert params["z"] == pytest.approx(-463.919, abs=0.05)
+    assert params["rx"] == pytest.approx(-5.137, abs=0.003)
+    assert params["ry"] == pytest.approx(-1.474, abs=0.002)
+    assert params["rz"] == pytest.approx(-5.297, abs=0.002)
+    assert params["s"] == pytest.approx(-2.4232, abs=0.001)
+
+
+def test_residuals():
+    input = mgi_ferro_to_etrs89()
+    input["source"]["pm"] = "ferro"
+    sol = hc.helmert_calc(input)
+    residuals = hc.compute_residuals(input, sol)
+    assert residuals["rms_2D"] < 2e-3
+    assert len(residuals["pipeline_2D"]) == 5
+    assert len(residuals["pipeline_3D"]) == 5
+    for j in range(0, 5):
+        for i in range(0, 3):
+            assert math.fabs(residuals["pipeline_3D"][j][i]) < 2e-3
+
+
+def test_cli():
+    dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    runner = os.path.join(dir_path, "helmert_calc.py")
+    example = os.path.join(dir_path, "example.json")
+    command = f'python3 "{runner}" --input "{example}"'
+    args = shlex.split(command)
+    p = subprocess.Popen(
+        args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE
+    )
+    assert any("helmert" in str(x.strip()) for x in p.stdout)
+    assert any("residuals" in str(x.strip()) for x in p.stdout)
+    assert p.wait() == 0
+
+
+def test_cli_outfile():
+    tmp = tempfile.NamedTemporaryFile(delete=False)
+    try:
+        dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        runner = os.path.join(dir_path, "helmert_calc.py")
+        example = os.path.join(dir_path, "example.json")
+        command = (
+            f'python3 "{runner}" --input "{example}" --output "{tmp.name}" --inverse -q'
+        )
+        args = shlex.split(command)
+        p = subprocess.Popen(
+            args,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            stdin=subprocess.PIPE,
+        )
+        assert p.wait() == 0
+    finally:
+        tmp.close()
+        os.unlink(tmp.name)
